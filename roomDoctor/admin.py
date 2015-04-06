e13d590a4c70036b45ce922b1e4e7db52607572f
Luis Naranjo <luisnaranjo733@gmail.com>
2015-04-06 16:50:47 -0700

    moving to flask

--- roomDoctor/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from roomDoctor.models import Room, Person
-
-admin.site.register(Room)
-admin.site.register(Person)
-
--- selectionTool/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-
-def home(request):
-    return HttpResponse('/op')
