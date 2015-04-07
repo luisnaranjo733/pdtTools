d3aad3dfc8dc426f8dc539335f8528e1aa72eadc
Luis Naranjo <luisnaranjo733@gmail.com>
2015-04-06 17:55:52 -0700

    Added SQLAlchemy Declarative

--- pdtTools/__init__.py
+++ pdtTools/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 # Import the view module after the application object is created.
 import pdtTools.views
+from pdtTools.database import db_session
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 
 
