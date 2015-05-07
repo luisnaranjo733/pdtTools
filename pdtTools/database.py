835ccbf7022f9d8986a11010db553c01e8112d29
Luis Naranjo <luisnaranjo733@gmail.com>
2015-05-06 21:59:10 -0700

    Added cross platform database path

--- pdtTools/database.py
+++ pdtTools/database.py
@@ -1,8 +1,23 @@
+import platform
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:////home/luis/Dropbox/pdtTools/test.db', convert_unicode=True)
+dist = platform.dist()[0]
+
+path = 'sqlite:///'
+
+if dist == 'centos':
+    temp = os.path.expanduser('~')
+    temp = os.path.join(temp, 'webapps')
+    temp = os.path.join(temp, 'phidelttools')
+    temp = os.path.join(temp, 'pdtTools')
+    temp = os.path.join(temp, 'test.db')
+    path += temp
+else:
+    path += '/home/luis/Dropbox/pdtTools/test.db'
+engine = create_engine(path, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=True,
                                          bind=engine))
