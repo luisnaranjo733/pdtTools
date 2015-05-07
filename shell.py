f30e3611a951b12155f87673276afed93bfa939a
Luis Naranjo <luisnaranjo733@gmail.com>
2015-05-06 19:16:26 -0700

    Improved shell script

--- shell.py
+++ shell.py
@@ -6,35 +6,45 @@ from pdtTools.models import User, Job
 
 init_db()
 
-def create_users():
-    luis = User(name='Luis', phone='206-478-4652')
-    tyler = User(name='Tyler', phone='4259714405')
-    michael = User(name='Michael', phone='206 520 1234')
-
-    db_session.add(luis)
-    db_session.add(tyler)
-    db_session.add(michael)
-    db_session.commit()
-    return luis, tyler, michael
-
-
 if __name__ == '__main__':
-    luis, tyler, michael = create_users()
+    if len(argv) == 1:
+        print('Create is -c')
+        print('List is -c')
+        print('Delete is -d')
+        exit(1)
 
-    print 'Users: %r' % User.query.all()
+    flag = argv[1]
 
-    job = Job()
-    job.date = date.today()
-    job.addWorker(luis)
-    job.addWorker(tyler)
-    
-    db_session.add(job)
-    db_session.flush()
+    if flag == '-c':
+        luis = User(name='Luis', phone='206-478-4652')
+        tyler = User(name='Tyler', phone='4259714405')
+        michael = User(name='Michael', phone='206 520 1234')
 
-    print 'Workers: %r' % job.getWorkers()
+        db_session.add(luis)
+        db_session.add(tyler)
+        db_session.add(michael)
+        db_session.flush()
 
-    db_session.commit()
-else:
-    job = Job.query.filter(Job.id == 1).first()
-    michael = User.query.filter(User.name == 'Michael').first()
-    #luis, tyler, michael = create_users()
+        job = Job()
+        job.date = date(2015, 5, 20)
+        job.addWorker(luis)
+        job.addWorker(tyler)
+        
+        db_session.add(job)
+
+        db_session.commit()
+    elif flag == '-l':
+        print('Users:')
+        for user in User.query.all():
+            print('\t%r' % user)
+        print('')
+
+        print('Jobs:')
+        for job in Job.query.all():
+            print('\tDate: %r' % job.date)
+            print('\tWorkers: %r' % [worker for worker in job.getWorkers()])
+            print('')
+    elif flag == '-d':
+        User.query.delete()
+        Job.query.delete()
+        db_session.commit()
