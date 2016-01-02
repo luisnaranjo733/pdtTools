460f4cf9d31bd565138280af6938a6864bc91c7b
Luis Naranjo <luisnaranjo733@gmail.com>
2016-01-01 20:23:54 -0800

    added initial models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,103 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '<Role: %s>' % self.name
+
+
+class UserStatus(models.Model):
+    userID = models.ForeignKey(User)
+    isAlumni = models.BooleanField(default=False)
+    points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '<UserStatus: %d>' % (self.userID.first_name)
+
+
+class Quarter(models.Model):
+    SEASONS = (
+        ('Aut', 'Autumn'),
+        ('Win', 'Winter'),
+        ('Spr', 'Spring'),
+        ('Sum', 'Summer')
+    )
+
+    season = models.CharField(max_length=10, choices=SEASONS)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return '<Season: %s %d>' % (self.season, self.year)
+
+
+class Quarter_UserStatus(models.Model):
+    quarterID =  models.ForeignKey(Quarter)
+    userStatusID = models.ForeignKey(UserStatus)
+
+    def __str__(self):
+        return '<Quarter_UserStatus: %d, %d>' % (self.quarterID.pk, self.userStatusID.pk)
+
+
+class Room(models.Model):
+    capacity = models.IntegerField()
+    isInhabitable = models.BooleanField('Livable?')
+
+    def __str__(self):
+        return '<Room: %d>' % self.pk
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '<Role: %s>' % self.name
+
+
+class KitchenDuty(models.Model):
+    date = models.DateField()
+
+    def __str__(self):
+        return '<KitchenDuty: %r' % self.date
+
+
+class User_KitchenDuty(models.Model):
+    userID = models.ForeignKey(User)
+    kitchenDutyID = models.ForeignKey(KitchenDuty)
+
+    def  __str__(self):
+        return '<User_KitchenDuty: %d, %d>' % (self.userID.pk, self.kitchenDutyID.pk)
+
+
+class Day(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return '<Day: %s' % self.name
+
+
+class Chore(models.Model):
+    checker = models.ForeignKey(User, null=True)
+
+    name = models.CharField(max_length=20)
+    description =  models.CharField(max_length=255)
+    timeDue = models.TimeField()
+
+    def __str__(self):
+        return '<Chore: %s>' % self.name
+
+
+class Chore_Day(models.Model):
+    choreID = models.ForeignKey(Chore)
+    dayID = models.ForeignKey(Day)
+
+    def __str__(self):
+        return '<Chore_Day: %d>' % (self.choreID.pk, self.dayID.pk)
+
+
+        
--- core/views.py
+++ core/views.py
@@ -1,57 +1,8 @@
-from urllib import urlencode
-from email.utils import parseaddr
-
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse, Http404
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.contrib.auth import authenticate, login, logout
-from django.forms import EmailField
-from django.core.exceptions import ValidationError
 
-from roomDoctor.models import Person
-    
-def logIn(request):
-    params = {}
-    if request.method == 'POST':
-        redirectUrl = request.GET.get('next') or '/'
-        username = request.POST['email']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-
-                return HttpResponseRedirect(redirectUrl) # Redirect after POST
-            else:
-                return HttpReponse("Your account has been disabled!")
-        else:
-            return HttpResponse("Your username and password were wrong!")
-    return render(request, 'logIn.html', params)
-        
-def logOut(request):
-    logout(request)
-    return redirect('/')
-    
-def addUser(request):
-    if request.method == 'GET':
-        return render(request, 'addUser.html')
-    else:
-        return signUp(request)  
-    
-def signUp(request):
-    email = request.POST.get('email')
-    name = request.POST.get('name')
-    password = request.POST.get('password')
-    if not email or not name or not password or not isEmailValid(email):
-        return HttpResponse("Make sure you include all of the fields, and a valid email address!")
-    user = Person.objects.create_user(email, password=password)
-    user.setName(name)
-    user.save()
-    return logIn(request)
-    
-def isEmailValid( email ):
-    try:
-        EmailField().clean(email)
-        return True
-    except ValidationError:
-        return False
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
