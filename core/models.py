b1a9cac001b2499b702a6b807903e243724ad11b
Luis Naranjo <luisnaranjo733@gmail.com>
2016-04-05 18:02:03 -0700

    update to models

--- core/models.py
+++ core/models.py
@@ -4,58 +4,77 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Member(models.Model):
+    userID = models.OneToOneField(User, on_delete=models.CASCADE)
+    bondNumber = models.IntegerField(null=True, blank=True)
+    isAlumni = models.BooleanField(default=False)
+    
+    def __str__(self):
+        name = self.userID.get_full_name()
+        if self.isAlumni:
+            name += ' (ALUMNI)'
+        return name
+  
+
+class Quarter(models.Model):
+    SEASONS = (
+        ('Fall', 'Fall'),
+        ('Winter', 'Winter'),
+        ('Spring', 'Spring'),
+        ('Summer', 'Summer')
+    )
+
+    season = models.CharField(max_length=10, choices=SEASONS)
+    startDate = models.DateField()
+    endDate = models.DateField()
+
+    def __str__(self):
+        return self.getHandle() 
+        
+    def getHandle(self):
+        'A string representation of the season and year of the quarter'
+        return '%s %d' % (self.season, self.startDate.year) 
+
+
+class User_Quarter(models.Model):
+    memberID = models.ForeignKey(Member)
+    quarterID =  models.ForeignKey(Quarter)
+
+    points = models.IntegerField('House points', default=0, blank=True)
+    isLiveIn = models.BooleanField(default=True)
+
+    def __str__(self):
+        return '%s, %s' % (self.memberID.userID.username, self.quarterID.getHandle())
+
+
 class Role(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return '<Role: %s>' % self.name
+        return self.name
 
 
-class Room(models.Model):
-    capacity = models.IntegerField()
-    isInhabitable = models.BooleanField('Livable?', default=True)
-
+class User_Quarter_Role(models.Model):
+    userQuarterID = models.ForeignKey(User_Quarter)
+    roleID = models.ForeignKey(Role)
+    
     def __str__(self):
-        return '<Room: %d>' % self.pk
+        return '%s, %s, %s' % (self.roleID, self.userQuarterID, self.userQuarterID.memberID)
 
+# class Room(models.Model):
+#     capacity = models.IntegerField()
+#     isInhabitable = models.BooleanField('Livable?', default=True)
 
-class Quarter(models.Model):
-    SEASONS = (
-        ('Aut', 'Autumn'),
-        ('Win', 'Winter'),
-        ('Spr', 'Spring'),
-        ('Sum', 'Summer')
-    )
+#     def __str__(self):
+#         return '<Room: %d>' % self.pk
 
-    season = models.CharField(max_length=10, choices=SEASONS)
-    year = models.IntegerField()
+# class KitchenDuty(models.Model):
+#     date = models.DateField()
+#     quarterID =  models.ForeignKey(Quarter)
+#     workers = models.ManyToManyField(User)
 
-    def __str__(self):
-        return '<Quarter: %s>' % self.handle()
-
-    def handle(self):
-        return '%s%d' % (self.season, self.year)
-
-
-class User_Quarter(models.Model):
-    userID = models.ForeignKey(User)
-    quarterID =  models.ForeignKey(Quarter)
-    roomID = models.ForeignKey(Room)
-
-    points = models.IntegerField('House points')
-    isAlumni = models.BooleanField(default=False)
-
-    def __str__(self):
-        return '<User_Quarter: %s, %s, %d>' % (self.userID.username, self.quarterID.handle(), self.roomID.pk)
-
-
-class KitchenDuty(models.Model):
-    date = models.DateField()
-    quarterID =  models.ForeignKey(Quarter)
-    workers = models.ManyToManyField(User)
-
-    def __str__(self):
-        return '<KitchenDuty: %r' % self.date
+#     def __str__(self):
+#         return '<KitchenDuty: %r' % self.date
 
 
 # class User_KitchenDuty(models.Model):
@@ -66,23 +85,23 @@ class KitchenDuty(models.Model):
 #         return '<User_KitchenDuty: %d, %d>' % (self.userID.pk, self.kitchenDutyID.pk)
 
 
-class Day(models.Model):
-    name = models.CharField(max_length=10)
+# class Day(models.Model):
+#     name = models.CharField(max_length=10)
 
-    def __str__(self):
-        return '<Day: %s>' % self.name
+#     def __str__(self):
+#         return '<Day: %s>' % self.name
 
 
-class Chore(models.Model):
-    checker = models.ForeignKey(User, null=True, blank=True)
-    daysDue = models.ManyToManyField(Day)
+# class Chore(models.Model):
+#     checker = models.ForeignKey(User, null=True, blank=True)
+#     daysDue = models.ManyToManyField(Day)
 
-    name = models.CharField(max_length=100)
-    description =  models.CharField(max_length=255)
-    timeDue = models.TimeField()
+#     name = models.CharField(max_length=100)
+#     description =  models.CharField(max_length=255)
+#     timeDue = models.TimeField()
 
-    def __str__(self):
-        return '<Chore: %s>' % self.name
+#     def __str__(self):
+#         return '<Chore: %s>' % self.name
 
 
 # class Chore_Day(models.Model):
@@ -94,13 +113,9 @@ class Chore(models.Model):
 
 
 app_models = [
+    Member,
     Role,
-    Room,
     Quarter,
     User_Quarter,
-    KitchenDuty,
-    #User_KitchenDuty,
-    Day,
-    Chore,
-    #Chore_Day
+    User_Quarter_Role
 ]
