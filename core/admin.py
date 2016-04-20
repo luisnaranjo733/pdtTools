3a3f5f69dbdd00540d2bd6a3570f1b0379b7341f
Luis Naranjo <luisnaranjo733@gmail.com>
2016-04-20 15:21:15 -0700

    del

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import app_models
-
-
-for model in app_models:
-    admin.site.register(model)
--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, JsonResponse, Http404
-from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required
-
-def index(request):
-    context = {}
-    return render(request, 'index.html', context)
