c4bd292ae9b4650ef1dec2dcbc7bb64cc6176676
Luis Naranjo <luisnaranjo733@gmail.com>
2016-05-04 15:10:16 -0700

    fixed bugs

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
 var pdtApp = angular.module("PdtApp", ['ui.router']);
-pdtApp.controller("PdtCtrl", ["$state", function($scope, $state) {
-    // function to submit the form after all validation has occurred            
-    $scope.signIn = function(password) {
-        if (password == "drake1848") {
-            console.log("pawn");
-            //$state.go("main-ui");
-        } else if (password == "morrison1848") {
-            console.log("admin");
-            //$state.go("main-ui");
-        } else {
-            console.log("unauthorized");
-        }
-    };
+
+pdtApp.controller("LogInCtrl", function($scope, $state) {
+    $scope.logIn = function(password) {
+        console.log(password);
+    }
+});
+
+
+pdtApp.controller("housePointsCtrl", [function($scope) {
+
+
 }]);
 
 pdtApp.config(function($stateProvider, $urlRouterProvider) {
-
-    $stateProvider.state('home', {
+    //$urlRouterProvider.otherwise("/login");
+    $stateProvider.state('logIn', {
         url: "/",
-        templateUrl: "partials/home.html"
+        templateUrl: "partials/login.html",
+        controller: "LogInCtrl"
     })
 
 
-    $stateProvider.state('main-ui', {
+    $stateProvider.state('pointsView', {
         url: "/points",
-        templateUrl: "partials/points.html"
+        templateUrl: "partials/points.html",
+        controller: "housePointsCtrl"
     })
 })
