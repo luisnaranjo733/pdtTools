525b61f57b52a628faa5d7619bbfd88072ab49fa
Luis Naranjo <luisnaranjo733@gmail.com>
2016-05-04 01:20:05 -0700

    form submission work in progress

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
 var pdtApp = angular.module("PdtApp", ['ui.router']);
-pdtApp.controller("PdtCtrl", function($scope) {
-    
-});
+pdtApp.controller("PdtCtrl", ["$state", function($scope, $state) {
+    // function to submit the form after all validation has occurred            
+    $scope.signIn = function(password) {
+        if (password == "drake1848") {
+            console.log("pawn");
+            //$state.go("main-ui");
+        } else if (password == "morrison1848") {
+            console.log("admin");
+            //$state.go("main-ui");
+        } else {
+            console.log("unauthorized");
+        }
+    };
+}]);
 
 pdtApp.config(function($stateProvider, $urlRouterProvider) {
 
     $stateProvider.state('home', {
         url: "/",
         templateUrl: "partials/home.html"
+    })
+
+
+    $stateProvider.state('main-ui', {
+        url: "/points",
+        templateUrl: "partials/points.html"
     })
 })
